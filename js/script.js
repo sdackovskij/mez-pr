ecc28434fdc9cf7e550b61dfb77c0c0987d740c8
sdackovskij <sdackovskij@gmail.com>
2019-05-06 21:12:18 +0300

    Header

--- js/script.js
+++ js/script.js
@@ -1,7 +1,49 @@
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
 
-    if (scroll >= 200) {
-    console.log('jquery is a piece of shit');
-    } 
+    if (scroll > 50) { 
+$('header').addClass('scroll'); 
+} 
+else { 
+$('header').removeClass('scroll') 
+} 
+});
+
+/*$('.your-class').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 1000
+}); */
+
+$('.your-class').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
 });
